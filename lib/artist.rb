c76a764dfd3836e8bf4efeb784753ff576fda070
Cody Rickman <cody@creativemindstudios.com>
2019-01-15 18:54:37 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,19 +3,22 @@ class Artist
   def initialize(name)
     @name = name
     @songs = []
+    @@songs = 0
   end
   def add_song(song)
     @songs << song
     song.artist = self
+
   end
   def add_song_by_name(name)
     song = Song.new(name)
     self.add_song(song)
+    @@songs += 1
   end
   def songs
     @songs
   end
   def self.song_count
-    return @songs.count
+    @@songs
   end
 end
